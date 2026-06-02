876ac4cfc92ed3ae5367b814db2a5058c781dfa3
heresemy <heresemy7@gmail.com>
2026-06-02 22:27:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -142,7 +142,7 @@ def handle_requests():
     server_name = request.args.get("server_name", "").upper()
     key = request.args.get("key")
 
-    if key != "Raihan":
+    if key != "SEMY50":
         return jsonify({"error": "Invalid or missing API key 🔑"}), 403
 
     if not uid or not server_name:
